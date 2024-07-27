588f12f2cff3ccd529ac00d9542bc4bddc8cac94
Marek Rubczewski <m.rubczewski@gmail.com>
2024-07-27 22:07:39 +0200

    Project setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('modular gen art');
+const helloWorldElement = document.getElementById('hello-world');
+if (helloWorldElement)
+    helloWorldElement.textContent = 'Hello, World!';
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+console.log('modular gen art')
+
+const helloWorldElement = document.getElementById('hello-world')
+if (helloWorldElement) helloWorldElement.textContent = 'Hello, World!'
